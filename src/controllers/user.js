92269d5cecb4122ae4fb884a2870838cbebafab4
Ahmed-Mohamed-Hassen-Mohamed <93393919+Ahmed-Mohamed-Hassen-Mohamed@users.noreply.github.com>
2023-10-14 01:00:01 +0300

    Update user.js

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -34,6 +34,7 @@ exports.createUser = async (req, res) => {
         .status(404)
         .send({ Error: "authorizedError", message: "Unauthorized" });
     }
+    res.status(200).send(req.body);
     await user.save();
     const token = user.generateToken();
     res.status(200).send({ user, token });
