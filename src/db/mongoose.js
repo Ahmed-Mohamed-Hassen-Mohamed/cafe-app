19df95f8c6ec7a7a86606afa73f19c51ee4095b1
Ahmed-Mohamed-Hassen-Mohamed <93393919+Ahmed-Mohamed-Hassen-Mohamed@users.noreply.github.com>
2023-10-14 11:00:17 +0300

    Update mongoose.js

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 mongoose
   .connect(process.env.DB_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
   })
   .then(() => {
     console.log("Connected to the database");
